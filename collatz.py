da00ef8b94771f0b65e7067ac10ef7edaa107082
ndotkarkut <33797028+ndotkarkut@users.noreply.github.com>
2018-07-18 16:51:21 +0200

    Create collatz.py

---
+++ collatz.py
@@ -0,0 +1,18 @@
+import sys
+
+def collatz(number):
+    if number != 1:
+        if number % 2 == 0:
+            num = number // 2
+            print num
+            return collatz(num)
+        elif number % 2 == 1:
+            num =  3 * number + 1
+            print num
+            return collatz(num)
+        else:
+            sys.exit
+    else:
+        print 'Collatz sequence complete!'
+
+collatz(input('What\'s your starting number? '))
